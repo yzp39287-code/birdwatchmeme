a14631964439b7beba7d9baecc9b369f3f75ba69
杨志鹏 <yzp39287@gmail.com>
2026-09-07 10:36:31 +0800

    Connect website to Supabase

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BIRDMEME_CONFIG = {
-  supabaseUrl: "YOUR_SUPABASE_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  supabaseUrl: "https://xlfpyzvuadqfekluwrdv.supabase.co",
+  supabaseAnonKey: "sb_publishable_xrB_nrHZNAqkPQK5OELb4g_hpez-0lx",
   adminGithubLogin: "yzp39287-code"
 };
