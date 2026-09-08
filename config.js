3292c46caeb025248b4d1d10677cb8e1f0cbee75
杨志鹏 <yzp39287@gmail.com>
2026-09-08 21:14:49 +0800

    Configure fixed administrator user ID

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BIRDMEME_CONFIG = {
-  supabaseUrl: "https://xlfpyzvuadqfekluwrdv.supabase.co",
-  supabaseAnonKey: "sb_publishable_xrB_nrHZNAqkPQK5OELb4g_hpez-0lx",
-  adminGithubLogin: "yzp39287-code"
+  supabaseUrl: "YOUR_SUPABASE_URL",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  adminUserId: "e06f43d2-f022-43ec-87bc-9d0b36ce5b4f"
 };
